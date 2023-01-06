23629d92b2e0a74eeb9456f62d67b8393ec9b2b1
valeriydev <vileriy.liashuk@free2ex.com>
2023-01-06 14:05:34 +0300

    feat: test first-worker deploy

--- first-worker/src/index.ts
+++ first-worker/src/index.ts
@@ -25,6 +25,6 @@ export default {
 		env: Env,
 		ctx: ExecutionContext
 	): Promise<Response> {
-		return new Response("This is first worker test action x4!");
+		return new Response("This is first worker test action x5!");
 	},
 };
